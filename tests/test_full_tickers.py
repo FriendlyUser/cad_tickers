0bce0426aadb18abf9444be6a75a40d7d7f9719a
dli <davidli012345@gmail.com>
2020-10-29 21:05:11 -0700

    increasing test coverage

---
+++ tests/test_full_tickers.py
@@ -0,0 +1,5 @@
+from cad_tickers.exchanges.all_tickers import mk_full_tickers
+
+
+def test_source():
+    tickers = mk_full_tickers()
--- tests/test_sedar_tsx.py
+++ tests/test_sedar_tsx.py
@@ -5,3 +5,9 @@ def test_get_tsx_docs():
 
     data = get_ticker_filings("ART")
     assert len(data.get("filings")) > 0
+
+
+def test_fake_doc():
+    data = get_ticker_filings("DART")
+    print(data)
+    assert data == None
