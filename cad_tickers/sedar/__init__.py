fb670103e2abda2c06ab9f76144d985723eba42e
dli <davidli012345@gmail.com>
2020-11-11 20:49:44 -0800

    fixing faulty documentation and parameter passing

---
+++ cad_tickers/sedar/__init__.py
@@ -0,0 +1,2 @@
+from cad_tickers.sedar.cse import get_cse_ticker_data, get_cse_sedar_docs
+from cad_tickers.sedar.tsx import get_ticker_filings
--- cad_tickers/sedar/tsx.py
+++ cad_tickers/sedar/tsx.py
@@ -6,20 +6,23 @@ from typing import Union
 
 def get_ticker_filings(
     symbol: str,
-    start_date: str = datetime.today().replace(day=1).strftime("%Y-%m-%d"),
-    end_date: str = datetime.today().strftime("%Y-%m-%d"),
+    fromDate: str = datetime.today().replace(day=1).strftime("%Y-%m-%d"),
+    toDate: str = datetime.today().strftime("%Y-%m-%d"),
     limit: int = 100,
 ) -> Union[dict, None]:
     """
     Parameters:
         symbol - ticker symbol from tsx, no prefix
+        fromDate - start date to grab documents
+        toDate - end date to grab documents
+        limit - max number of documents to retrieve
     Returns:
         dict - :ref:`Quote By Symbol <quote_by_symbol_query>`
     """
     payload = GQL.get_company_filings_payload
     payload["variables"]["symbol"] = symbol
-    payload["variables"]["start_date"] = start_date
-    payload["variables"]["end_date"] = end_date
+    payload["variables"]["fromDate"] = fromDate
+    payload["variables"]["toDate"] = toDate
     payload["variables"]["limit"] = limit
     url = "https://app-money.tmx.com/graphql"
     r = requests.post(
@@ -41,7 +44,7 @@ def get_ticker_filings(
 
 
 if __name__ == "__main__":
-    import json
-    import os
-
-    cmc = get_ticker_filings("ART")
+    art = get_ticker_filings(
+        "ART", start_date="2015-11-11", end_date="2020-11-11", limit=108
+    )
+    print(art)
