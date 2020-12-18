c0735b21327b7d527966f2b9a3a1fe92382aff21
David Li <davidli012345@gmail.com>
2020-12-18 14:18:18 -0800

    fixing test cases

--- tests/news/test_ceo.py
+++ tests/news/test_ceo.py
@@ -3,5 +3,5 @@ from cad_tickers.news.ceo import SearchParams, ceo_url, params_to_dict
 def test_create_sp():
     default_sp = SearchParams(until=1608318681)
     y = params_to_dict(default_sp)
-    x = {'channel': '@newswire', 'load_more': 'top', 'original_scroll_height': 0, 'until': 1608318681, 'filter[terms]': 'APHA', 'filter[top]': 100}
+    x = {'channel': '@newswire', 'load_more': 'top', 'original_scroll_height': 0, 'until': 1608318681, 'filters[terms]': 'APHA', 'filters[top]': 100}
     assert y == x
