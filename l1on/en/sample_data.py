b31f59155e938850edcf690f748b2db33b1e3761
David Li <davidli012345@gmail.com>
2021-08-18 10:03:59 -0700

    New translations sample_data.py (English, Canada)

---
+++ l1on/en/sample_data.py
@@ -0,0 +1,230 @@
+ref_text = """
+
+(TheNewswire)
+
+
+Montreal – TheNewswire
+- November 25, 2020 Ecolomondo
+Corporation (TSXV:ECM) (the “Company” or “Ecolomondo”)
+releases its unaudited interim consolidated financial statements and
+notes hereto and its related management discussion & analysis
+(“MD&A”) for the interim period ended September 30, 2020 (the
+“Interim Financial
+Statements”). 
+
+
+During the period, the Company made considerable
+progress on the construction of its new Hawkesbury Thermal
+Decomposition (“TDP”) turnkey facility. Management estimates
+that construction of the building that will house the plant is about
+90% complete and expects completion by the end of 2020. Installation
+of the plant equipment began in late August of 2020 and is proceeding
+according to schedule while commissioning is now scheduled to begin in
+the first quarter of 2021. 
+
+
+
+
+FLARE AND GAS STORAGE TANK
+
+
+ 
+
+
+
+ 
+
+SHREDDING LINE
+
+
+
+
+ 
+
+RECYCLED CARBON BLACK PROCESSING EQUIPMENT
+
+
+
+
+ 
+
+GAS SCRUBBING EQUIPMENT
+
+
+To view further updates of the Hawkesbury TDP facility,
+please visit the Company’s website at www.ecolomondo.com. 
+
+
+As of September 30, 2020, capital expenditures for the
+Hawkesbury facility totalled $19,965,404 and were essentially for site
+purchase, engineering, site preparation, building construction, labor,
+permitting, equipment purchases and installation. Management believes
+that the project will be completed on budget and expects to begin
+commercial operations by the end of April of 2021.
+
+
+During the quarter ended September 30, 2020, the
+Company continued to ready its Pilot plant, located in Contrecoeur,
+Quebec, Canada, a 2-reactor industrial-size TDP facility, to operate
+as the training center to train future operators needed for the
+thermal processing department at its new TDP turnkey facility in
+Hawkesbury.
+
+
+The Company continued to market all of its end-products
+and to secure offtake agreements for them, especially its recovered
+carbon black, known as “Mondo Black”. The Company was also quite
+active in marketing TDP turnkey facilities, in Canada, USA and Europe.
+
+
+
+It also continued to improve its technology platform
+and make important progress in the areas of process optimization,
+efficiency, safety and end-product quality. Once built, the Hawkesbury
+facility will be comprised of four different processing departments
+(shredding, thermal processing, recovered carbon black processing, oil
+fractionation) and, once fully operational, this facility is expected
+to process a minimum of 14,000 tons of tire waste per year and produce
+5,300 tons of recovered carbon black, 42,700 barrels of oil, 1,800
+tons of steel, 1,600 tons of process gas and 850 tons of fiber.
+
+
+
+During this period, the Company worked aggressively to
+complete the construction of its new Hawkesbury TDP turnkey facility.
+Once completed, this facility will be a first of its kind and be used
+as a technological showpiece to promote the sale of TDP turnkey
+facilities globally. Management believes that once the Hawkesbury facility comes online,
+it is expected to be a testimonial of the commercial viability of TDP
+turnkey facilities and, as a
+result, it is expected to promote the sale of other TDP turnkey
+facilities.
+
+
+Impact of the COVID-19
+Pandemic
+
+
+The world economy is still impacted by the crisis
+created by the COVID-19 (coronavirus) pandemic. The COVID-19 pandemic
+has resulted in the confinement of the population of Canada and many
+countries around the world.
+
+
+The Company has taken and will continue to take all
+necessary actions needed to reduce the impact of the crisis created by
+the coronavirus. The Company expects that the milestone to complete
+the plant under construction in Hawkesbury, Ontario, will not be
+materially impacted by the current situation. The Company has experienced some
+delays in lead times for certain parts, and some work slowdown by
+subcontractors due to labor shortages. It is to
+note, it is impossible to determine with certitude the long-term
+consequences and financial implications if the current situation
+persists. 
+
+
+About Ecolomondo Corporation
+
+
+Ecolomondo is a cleantech Canadian company that is
+commercializing its waste-to-products technology. The Thermal
+Decomposition Process (“TDP”) converts hydrocarbon waste into
+marketable commodity end-products, namely carbon black substitute,
+oil, gas and steel. Technologies such as Ecolomondo’s are expected
+to play an important role in resource recovery needed in today’s
+circular economy. 
+
+
+The Company’s main revenues will come from the sale
+of TDP turnkey facilities and royalties from their operations. TDP
+facilities will generate revenues from the sale of end-products,
+tipping fees and carbon credits. Ecolomondo’s first focus is to
+market TDP turnkey facilities that use scrap tires as a feedstock,
+because scrap tires yield end-products with a higher commercial value,
+especially the recycled carbon black.
+
+
+Management believes that technological breakthroughs in
+areas of emissions, process optimization, automation, end-product
+processing and safety, position Ecolomondo to become an industry
+leader in the resource recovery of hydrocarbon waste. 
+
+
+Cautionary Note Regarding Forward
+Looking Statements 
+
+
+The information in this news release includes certain
+information and statements about management's view of future events,
+expectations, plans and prospects that constitute forward looking
+statements. These statements are based upon assumptions that are
+subject to significant risks and uncertainties. Because of these risks
+and uncertainties and as a result of a variety of factors, the actual
+results, expectations, achievements or performance may differ
+materially from those anticipated and indicated by these forward
+looking statements. Although Ecolomondo believes that the expectations
+reflected in forward looking statements are reasonable, it can give no
+assurances that the expectations of any forward looking statements
+will prove to be correct. Except as required by law, Ecolomondo
+disclaims any intention and assumes no obligation to update or revise
+any forward looking statements to reflect actual results, whether as a
+result of new information, future events, changes in assumptions,
+changes in factors affecting such forward looking statements or
+otherwise.
+
+
+For further information please
+contact:
+
+
+Elio Sorella, 
+
+
+Chairman and Chief Executive Officer, Ecolomondo
+
+
+Tel: (450) 587-5999
+
+
+esorella@ecolomondocorp.com
+
+
+www.ecolomondo.com
+
+
+Neither TSX Venture Exchange nor its
+Regulation Services Provider (as that term is defined in the policies
+of the TSX Venture Exchange) accepts responsibility for the adequacy
+or accuracy of this release.
+
+
+NOT FOR DISTRIBUTION TO U.S NEWSWIRE SERVICES OR FOR
+DISSEMINATION IN THE UNITED STATES
+
+Copyright (c) 2020 TheNewswire - All rights reserved.
+"""
+
+sample_spiel = {
+    'channel': 'newsroom',
+    'spiel': "Aphria Inc. Recognized for Executive Gender Diversity by Globe and Mail's Women Lead Here Benchmark and Provides an Update on COVID-19 @newswire/aphria-inc-recognized-for-executive-gender-diversity $APHA-US $APHA #news/pharma #news/healthcare",
+    'name': '@newswire',
+    'timestamp': 1585310926647,
+    'spiel_id': '0a6d390dc0ef',
+    'color': '#666   ',
+    'public_id': None, 
+    'parent_id': '0a6d390dc0ef',
+    'parent_channel': 'newsroom',
+    'parent_timestamp': 1585310926647,
+    'votes': None,
+    'editable': False,
+    'featured': None,
+    'verified': False,
+    'fake': False,
+    'bot': None,
+    'voted': False,
+    'flagged': False,
+    'own_spiel': False,
+    'score': None, 
+    'saved_id': None,
+    'saved_timestamp': None
+}   
