90458b48069f7a27673143cea80c5c369c4865f0
David Li <davidli012345@gmail.com>
2021-08-18 10:05:13 -0700

    New translations __init__.py (Chinese Traditional, Hong Kong)

--- l1on/zh/__init__.py
+++ l1on/zh/__init__.py
@@ -1,16 +1,3 @@
-"""
-
-Stock News
-----------
-
-Downloads stock ticker news from yahoo such as from https://ca.finance.yahoo.com/quote/IP.CN/
- 
-
-IIROC - Investment Industry Regulatory Organization of Canada (IIROC) contains 
-information about stock halts
-
-https://www.iiroc.ca/Pages/default.aspx
-"""
-
-from cad_tickers.news.iiroc_halts import *
-from cad_tickers.news.stock_news import *
+__version__ = "1.4.1"
+__author__ = "David Li <davidli012345@gmail.com>"
+# from cad_tickers.exchanges import *
