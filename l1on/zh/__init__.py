e00280aa957e77ca7a47190346d9789154c4d33e
David Li <davidli012345@gmail.com>
2021-08-18 10:05:16 -0700

    New translations __init__.py (Chinese Traditional, Hong Kong)

--- l1on/zh/__init__.py
+++ l1on/zh/__init__.py
@@ -1,3 +1,17 @@
-__version__ = "1.4.1"
-__author__ = "David Li <davidli012345@gmail.com>"
-# from cad_tickers.exchanges import *
+"""
+Utilities
+----------
+
+Various Utility Functions such as transforming tickers from cse to yahoo
+"""
+from cad_tickers.util.utils import (
+    transform_name_to_slug,
+    parse_description_tags,
+    extract_recent_news_links,
+    make_cse_path,
+    read_df_from_file,
+    tickers_to_ytickers,
+    cse_ticker_to_yahoo,
+    tsx_ticker_to_yahoo,
+    cse_ticker_to_webmoney
+)
