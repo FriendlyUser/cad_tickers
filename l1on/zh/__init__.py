8e5f81bd0f0cad7808fc057a126b686ef48f9fa8
David Li <davidli012345@gmail.com>
2021-08-18 10:05:01 -0700

    New translations __init__.py (Chinese Traditional, Hong Kong)

--- l1on/zh/__init__.py
+++ l1on/zh/__init__.py
@@ -1,24 +1,16 @@
 """
-Exchanges
+
+Stock News
 ----------
 
-Downloading and cleaning data from the cse and tsx exchanges
+Downloads stock ticker news from yahoo such as from https://ca.finance.yahoo.com/quote/IP.CN/
+ 
+
+IIROC - Investment Industry Regulatory Organization of Canada (IIROC) contains 
+information about stock halts
+
+https://www.iiroc.ca/Pages/default.aspx
 """
 
-from cad_tickers.exchanges.cse import (
-    get_cse_files,
-    get_cse_tickers_df,
-    get_description_for_url,
-    get_all_cse_tickers,
-)
-from cad_tickers.exchanges.tsx import (
-    get_all_tickers_data,
-    get_all_tsx_tickers,
-    get_ticker_data,
-    get_tickers,
-    get_tsx_tickers,
-    gql_data,
-)
-from cad_tickers.exchanges.classes import CSETicker, CSESedarFilings
-
-from cad_tickers.exchanges.all_tickers import mk_full_tickers
+from cad_tickers.news.iiroc_halts import *
+from cad_tickers.news.stock_news import *
