53815137cd16eb2b097bad0626f2688d1e1b7cb3
David Li <davidli012345@gmail.com>
2021-08-18 10:05:23 -0700

    New translations __init__.py (English, Canada)

--- l1on/en/__init__.py
+++ l1on/en/__init__.py
@@ -1,17 +1,2 @@
-"""
-Utilities
-----------
-
-Various Utility Functions such as transforming tickers from cse to yahoo
-"""
-from cad_tickers.util.utils import (
-    transform_name_to_slug,
-    parse_description_tags,
-    extract_recent_news_links,
-    make_cse_path,
-    read_df_from_file,
-    tickers_to_ytickers,
-    cse_ticker_to_yahoo,
-    tsx_ticker_to_yahoo,
-    cse_ticker_to_webmoney
-)
+from cad_tickers.sedar.cse import get_cse_ticker_data, get_cse_sedar_docs
+from cad_tickers.sedar.tsx import get_ticker_filings
